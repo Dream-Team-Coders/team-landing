8c367affa35017dee05f1c6f0cefd4c980496f69
Mikhail <leonovm143@gmail.com>
2023-04-16 16:10:35 +0600

    section-1 done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,13 @@ import BgIcon1 from '../../public/icons/bg-icon-1.svg'
 import BgIcon2 from '../../public/icons/bg-icon-2.svg'
 import Button from '@/components/Button'
 import ChartIcon from '../../public/icons/chart-icon.svg'
+import City from '../../public/images/section-1-1.png'
 import HardhatIcon from '../../public/icons/hardhat-icon.svg'
+import Image from 'next/image'
 import ItemBlock from '@/components/ItemBlock'
 import MainLayout from '@/layouts/MainLayout'
 import RecyclingIcon from '../../public/icons/recycling-icon.svg'
+import Transport from '../../public/images/section-1-2.png'
 
 export default function Home() {
     return (
@@ -23,8 +26,12 @@ export default function Home() {
                             title='Подробнее'
                         />
                     </div>
+                    <div className='flex flex-col justify-center'>
+                        <Image className='mt-20' src={City} alt='City'/>
+                        <Image className='mt-9' src={Transport} alt='Transport' />
+                    </div>
                 </section>
-                <section className='w-full flex justify-between'>
+                <section className='w-full flex justify-between mt-36'>
                     <ItemBlock
                         iconBackground={BgIcon1}
                         icon={ChartIcon}
