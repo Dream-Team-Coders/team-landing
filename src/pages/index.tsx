9026971704bf9f3d405a84d3f3f281169f5f5890
Mikhail <leonovm143@gmail.com>
2023-04-16 16:00:00 +0600

    font fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,28 @@
-import BgIcon1 from '../../public/bg-icon-1.svg'
-import BgIcon2 from '../../public/bg-icon-2.svg'
-import ChartIcon from '../../public/chart-icon.svg'
-import HardhatIcon from '../../public/hardhat-icon.svg'
+import BgIcon1 from '../../public/icons/bg-icon-1.svg'
+import BgIcon2 from '../../public/icons/bg-icon-2.svg'
+import Button from '@/components/Button'
+import ChartIcon from '../../public/icons/chart-icon.svg'
+import HardhatIcon from '../../public/icons/hardhat-icon.svg'
 import ItemBlock from '@/components/ItemBlock'
 import MainLayout from '@/layouts/MainLayout'
-import RecyclingIcon from '../../public/recycling-icon.svg'
+import RecyclingIcon from '../../public/icons/recycling-icon.svg'
 
 export default function Home() {
     return (
         <main className='flex flex-col items-center'>
             <MainLayout>
-                <section>
-
+                <section className='w-full flex flex-col items-center justify-center mt-[74px]'>
+                    <span className='font-inter text-5xl max-w-[820px] text-center'>
+                        Умная доставка материалов для сложных строительных площадок
+                    </span>
+                    <div className='flex flex-row mt-8 gap-x-4'>
+                        <Button
+                            title='Заказать демо'
+                            isDark />
+                        <Button 
+                            title='Подробнее'
+                        />
+                    </div>
                 </section>
                 <section className='w-full flex justify-between'>
                     <ItemBlock
