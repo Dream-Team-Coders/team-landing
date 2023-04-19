efa902dd88bc0fccca8f7b5a6e399a916cd50887
vibin17 <60400627+vibin17@users.noreply.github.com>
2023-04-19 03:00:09 +0000

    Deploying to gh-pages from @ Dream-Team-Coders/team-landing@cdca492c3b9ff966df1c71251f205eeff9705255 🚀

---
+++ _next/static/Sz6O4ShaN6aTt1DIw5zFz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1ad8be9c5a77a88d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
